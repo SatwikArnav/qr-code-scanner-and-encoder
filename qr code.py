a81db360cd1a4594fa93e09c9cff963626df71b8
SatwikArnav <122200676+SatwikArnav@users.noreply.github.com>
2023-05-15 14:03:16 +0530

    Add files via upload

---
+++ qr code.py
@@ -0,0 +1,25 @@
+import pyqrcode
+import png
+from PIL import ImageTk, Image
+from pyqrcode import QRCode
+import tkinter as tk 
+from tkinter import *
+import PIL.Image
+
+
+window.title('Python Calculator')
+
+Label(window,text='Lets Create QR Code',font='arial 15').pack()
+
+s = tk.StringVar()
+  
+
+def create_qrcode():
+    s1=s.get()
+    qr = pyqrcode.create(s1)
+    qr.png('myqr.png', scale = 6)
+    Label(window,text='QR Code is created and saved successfully').pack()
+
+Entry(window,textvariable=s,font='arial 15').pack()
+Button(window,text='create',bg='pink',command=create_qrcode).pack()
+window.mainloop()
